00a011b62cbce785c9b6b58621d925e21ceb8f38
Jeppe Hansen <jeppha@mmmi.sdu.dk>
2026-08-14 09:27:37 +0200

    fixed namespace issue with teleop.launch.py

--- robot_bringup/launch/teleop.launch.py
+++ robot_bringup/launch/teleop.launch.py
@@ -1,40 +1,45 @@
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, ExecuteProcess
+from launch.actions import DeclareLaunchArgument, ExecuteProcess, OpaqueFunction
 from launch.substitutions import LaunchConfiguration
+
+
+def launch_teleop(context):
+    namespace = LaunchConfiguration('namespace').perform(context)
+
+    cmd_vel_topic = '/diff_drive_controller/cmd_vel'
+    
+    if namespace:
+        cmd_vel_topic = f'/{namespace}{cmd_vel_topic}'
+
+    return [
+        ExecuteProcess(
+            cmd=[
+                'gnome-terminal',
+                '--',
+                'ros2',
+                'run',
+                'teleop_twist_keyboard',
+                'teleop_twist_keyboard',
+                '--ros-args',
+                '--remap',
+                f'cmd_vel:={cmd_vel_topic}',
+                '-p',
+                'stamped:=true',
+            ],
+            output='screen',
+        )
+    ]
 
 
 def generate_launch_description():
 
     declare_namespace = DeclareLaunchArgument(
         'namespace',
-        default_value='robot1',
+        default_value='',
         description='Namespace of the robot to control'
-    )
-
-    namespace = LaunchConfiguration('namespace')
-
-    teleop = ExecuteProcess(
-        cmd=[
-            'gnome-terminal',
-            '--',
-            'ros2',
-            'run',
-            'teleop_twist_keyboard',
-            'teleop_twist_keyboard',
-            '--ros-args',
-            '--remap',
-            [
-                'cmd_vel:=/',
-                namespace,
-                '/diff_drive_controller/cmd_vel'
-            ],
-            '-p',
-            'stamped:=true',
-        ],
-        output='screen',
     )
 
     return LaunchDescription([
         declare_namespace,
-        teleop,
+        OpaqueFunction(function=launch_teleop),
     ])
